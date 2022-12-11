c5b7757a16c0314d1b76a0c0cb81c0b4b0b7b9aa
Sevval Kurtgoz <sevval1834@outlook.com>
2022-12-11 18:19:59 +0300

    örnek tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,8 +1,61 @@
+import React from "react";
+import axios from "axios";
+
 function App() {
+  const [veri, veriGuncelle] = React.useState( { hits: [] } )
+  const [sorgu, sorguGuncelle] = React.useState( "book" )
+  const [arama, aramaGuncelle] = React.useState( "book" )
+  const [yukelniyor, yukleniyorGuncelle] = React.useState(false)
+  const [hata, hataGuncelle] = React.useState(false)
+
+
+  React.useEffect( ()=>{
+    async function veriCek() {
+      hataGuncelle(false)
+      yukleniyorGuncelle(true)
+    //  promise nesnesinden geriye ya başarılı yada olumsuz bir sinyal gelir
+    // olumsuz sonuçları try catch içinde yakalarız
+    try{
+      const sonuc = await axios("https://hn.algolia.com/api/v1/search?query=" + arama)
+      veriGuncelle(sonuc.data)
+    }catch(error){
+      hataGuncelle(false)
+    }
+    
+      yukleniyorGuncelle(false)
+    }
+
+    veriCek()
+  }, [arama] )
+ 
+
+  console.log("App Komponenti Render oldu!")
   return (
-    <div>
-      App
-    </div>
+    <section className="container py-5">
+      <div>
+        <input type="text" value={sorgu} onChange={ (olay)=>{sorguGuncelle(olay.target.value)} } />
+        <button onClick={ ()=>{ aramaGuncelle(sorgu) } }>Ara</button>
+        <button onClick={() => { console.log("Button Tıklandığında")}}>Tıkla</button>
+      </div>
+      {hata === true? <p className="bg-danger p-3">Bir hata var!</p> : ""}
+      {
+      yukelniyor === true?
+      (
+         <p>Yükleniyor...</p>
+      )
+      :
+      (
+      
+      <ul>
+        {
+          veri.hits.map( (eleman)=>{
+            return <li key={eleman.objectID}> <a href={eleman.url}>  {eleman.title} </a> </li>
+          } )
+        }
+      </ul>
+      )
+    }
+    </section>
   );
 }
 
